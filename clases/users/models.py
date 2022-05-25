b54abf7c641351d9d925b6a7068cdea495285c1a
Luis Aguirre <luis_aguirre_1994@outlook.com>
2022-05-25 17:16:54 -0500

    erasing imports

--- clases/users/models.py
+++ clases/users/models.py
@@ -1,17 +1,15 @@
 from email.policy import default
-from enum import unique
-from tkinter.tix import Tree
-from sqlalchemy import Boolean,Column,ForeignKey,Integer,String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from config.database import Base
 
+
 class User(Base):
     __tablename__ = "users"
-    
-    id = Column(Integer,primary_key=True,index=True,autoincrement=True)
-    email = Column(String,unique=True,index=True)
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    username = Column(String,unique=True)
-    isActive = Column(Boolean,default=True)
-    
+    username = Column(String, unique=True)
+    isActive = Column(Boolean, default=True)
